63630e8fe0ec46d38067e1c0053c94931215d9e5
GRD39101 <grobertdeaniii@gmail.com>
2025-01-19 15:43:50 -0500

    first portflio update.

--- portfolio.py
+++ portfolio.py
@@ -1,1 +1,5 @@
 import streamlit as st
+
+st.set_page_config(page_title="My webpage",page_icon = ":tada:",layout="wide")
+st.title('Hi there is my name is George Dean III :wave:')
+st.subheader("I am creating a portfolio website to showcase my skills and experience.")
