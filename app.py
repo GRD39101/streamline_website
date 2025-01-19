81732a56747de4c6f9abc1e8259caad024ea00cd
GRD39101 <grobertdeaniii@gmail.com>
2025-01-19 14:25:23 -0500

    added a question for users to add thier names.

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ st.title('Hi there my name is George Dean III :wave:')
 st.subheader("I am putting my python knowledge to test for both frontend and backend development somewhat to build websites and much more.")
 st.text("I am using streamlit to build a site.")
 st.text("On the 2nd go around, I will update this to ask you questions.")
+name = st.text_input("What is your name? Type it below...")
+if(st.button("Submit")):
+    result = name.title()
+    st.success(result)
 
