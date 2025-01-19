aaf4bd3603b3b962bbb4000f3e3613bce0015886
GRD39101 <grobertdeaniii@gmail.com>
2025-01-19 17:31:51 -0500

    add college info

--- portfolio.py
+++ portfolio.py
@@ -5,5 +5,8 @@ st.subheader("I am creating a portfolio website to showcase my skills and experi
 st.text("I am using streamlit to build a site.")
 
 st.markdown('<h2>College</h2>', unsafe_allow_html=True)
+st.text("Clemson University")
+st.text("Bachelors of Science in Computer Information Systems")
+st.text('08/2022 - 05/2026')
 st.markdown('<h2>Experience</h2>', unsafe_allow_html=True)
 st.markdown('<h2>Skills</h2>', unsafe_allow_html=True)
