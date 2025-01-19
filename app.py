b8564ae262aa515d18ea729504eb5602ac4f080a
GRD39101 <grobertdeaniii@gmail.com>
2025-01-19 14:02:58 -0500

    first development of website using streamit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="My webpage",page_icon = ":tada:",layout="wide")
+
+st.title('Hi there my name is George Dean III :wave:')
+st.subheader("I am putting my python knowledge to test for both frontend and backend development somewhat to build websites and much more.")
+st.text("I am using streamlit to build a site.")
+st.text("On the 2nd go around, I will update this to ask you questions.")
+
